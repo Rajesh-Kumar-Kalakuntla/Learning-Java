35eeb3457bb6cf87e699260e0a4b7719eeacf228
Rajesh Kumar <kalakuntlarajeshkumar@email.com>
2025-01-25 16:06:05 +0530

    datatypes learned

--- DataTypes.java
+++ DataTypes.java
@@ -1,1 +1,14 @@
-
+public class DataTypes {  	
+    public static void main(String[] args) { 		
+        //there are 7 primitive dataTypes 		
+        byte a = 1;
+        short b = 32767; 	
+        int c = 100; 		
+        long d =2000000; 		
+        float e = 200.78f; 		
+        double f = 3000000.78; 		
+        char g = 'a'; 		
+        boolean h = true; 		
+        System.out.println(a+" "+b +" "+c+" "+d+" "+e+" "+f+" "+g+" "+h); 
+    }         
+ }
